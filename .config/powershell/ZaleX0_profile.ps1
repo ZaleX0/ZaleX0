d2b3b70abafddb1154bd202311a0c954405b510f
Szymon Głowacki <pgszymon@gmail.com>
2023-04-08 11:23:13 +0200

    Add PowerShell profile

---
+++ .config/powershell/ZaleX0_profile.ps1
@@ -0,0 +1,8 @@
+# Prompt
+oh-my-posh init pwsh --config $env:USERPROFILE\.config\powershell\ZaleX0.omp.json | Invoke-Expression
+
+# Aliases
+function ll_alias { Get-ChildItem $args -Exclude .* | Format-Wide Name -AutoSize }
+Set-Alias -Name ll -Value ll_alias -Option AllScope
+Set-Alias vim nvim
+Set-Alias lg lazygit
